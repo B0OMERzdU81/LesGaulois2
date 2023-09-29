1606fc257f4ee5941cde7a7b4999a59da1321e1d
B0OMERzdU81 <114724278+B0OMERzdU81@users.noreply.github.com>
2023-09-29 09:18:41 +0200

    Mise en place d'assertions basique sur Romain

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -5,6 +5,7 @@ public class Romain {
 	private int force;
 	
 	public Romain(String nom, int force) {
+		assert force>0 : "La force du romain est inférieur à 1";
 		this.nom=nom;
 		this.force=force;
 	
@@ -22,6 +23,8 @@ public class Romain {
 	}
 	
 	public void recevoirCoup(int forceCoup) {
+		assert force>0: "La force du romain est déjà inférieur ou égale à 0";
+		int i=force;
 		force-=forceCoup;
 		if(force>0) {
 			parler("Aie");
@@ -29,7 +32,7 @@ public class Romain {
 		else {
 			parler("J'abandonne");
 		}
-		
+		assert i>force: "La force du romain n'a pas diminué, il a des joues en béton";
 	}
 	
 	
@@ -40,6 +43,7 @@ public class Romain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Romain jaximus = new Romain ("Jaximus", 5);
+		Romain minus = new Romain ("Minus", 6);
 		jaximus.prendreParole();
 		jaximus.parler("Je n'aime pas me faire frapper");
 		jaximus.recevoirCoup(1);

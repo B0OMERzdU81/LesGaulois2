0b342de4abd37fadf73ea2fc8fb721e7b9445b21
B0OMERzdU81 <114724278+B0OMERzdU81@users.noreply.github.com>
2023-09-29 09:44:45 +0200

    Début de la création de l'équipement

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,11 +3,14 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipement;
+	private int nbEquiment=0;
 	
 	public Romain(String nom, int force) {
 		assert force>0 : "La force du romain est inférieur à 1";
 		this.nom=nom;
 		this.force=force;
+		equipement= new Equipement[2];
 	
 	}
 	public String getNom() {
@@ -35,18 +38,48 @@ public class Romain {
 		assert i>force: "La force du romain n'a pas diminué, il a des joues en béton";
 	}
 	
-	
+	public void conditionEquipement(Equipement equiper) {
+		if(equipement[0]==equiper || equipement[1]==equiper){
+			System.out.println("Le soldat "+nom + " possède déjà un casque");
+			break;
+		}
+		else {
+			if(equipement[0]==null) {
+				equipement[0]=equipement;
+			}
+			if equipement
+				
+			}
+		
+	}
+	public void sEquiper(Equipement equiper) {
+		if (equipement[0]!=null && equipement[1]!= null) {
+			System.out.println("Le soldat "+nom + "est déjà bien protégé!");
+		}
+		else {
+			switch (equiper) {
+			case CASQUE:
+				conditionEquipement(equiper);
+				}
+				
+			}
+		}
+		
+	}
 	
 	
 	
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Romain jaximus = new Romain ("Jaximus", 5);
 		Romain minus = new Romain ("Minus", 6);
+		
 		jaximus.prendreParole();
 		jaximus.parler("Je n'aime pas me faire frapper");
 		jaximus.recevoirCoup(1);
+		
+		System.out.println(Equipement.BOUCLIER);
+		System.out.println(Equipement.CASQUE);
 	}
 
 }
